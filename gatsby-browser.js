332db953c69c41e64fd1d3587962dd79be1368f1
Luca Fabbri <luca@keul.it>
2020-02-02 10:38:55 +0100

    Styling changes partially cherry-picked from 84cd8046dd328811262f6924d280e1aaad688b7b

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,6 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+// require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/themes/prism-solarizedlight.css")
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -6,10 +6,37 @@ Wordpress2016.overrideThemeStyles = () => {
     "a.gatsby-resp-image-link": {
       boxShadow: `none`,
     },
-    ".gatsby-highlight pre": {
-      overflowX: 'auto',
-      maxWidth: '100%',
-    }
+    // ".gatsby-highlight pre": {
+    //   overflowX: 'scroll',
+    //   maxWidth: '100%',
+    // },
+    ".gatsby-highlight-code-line": {
+      backgroundColor: '#E4DDCA',
+      display: 'block',
+      marginRight: '-1em',
+      marginLeft: '-1em',
+      paddingRight: '1em',
+      paddingLeft: '0.75em',
+      borderLeft: '0.25em solid #f99',
+    },
+    ".gatsby-highlight": {
+      backgroundColor: '#fdf6e3',
+      borderRadius: '0.3em',
+      margin: '0.5em 0',
+      padding: '1em',
+      overflow: 'auto',
+    },
+    '.gatsby-highlight pre[class*="language-"]': {
+      backgroundColor: 'transparent',
+      margin: 0,
+      padding: 0,
+      overflow: 'initial',
+      float: 'left',
+      minWidth: '100%',
+    },
+    '.gatsby-highlight pre[class*="language-"].line-numbers': {
+      paddingLeft: '2.8em',
+    },
   }
 }
 
